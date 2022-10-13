b416630730c86a1ad1a2c55bae5216b8e48e4708
Pietro Visconti <visconti373@gmail.com>
2022-10-13 21:10:54 +0200

    Rails API setup and added JWT authentication standard

---
+++ app/controllers/user_token_controller.rb
@@ -0,0 +1,9 @@
+class UserTokenController < Knock::AuthTokenController
+  private
+  def auth_params
+    # Without overriding the auth_params here, you get "unpermitted
+    #   parameter" errors for username. The call seems to work anyway,
+    #   but this eliminates the error message from your logs.
+    params.require(:auth).permit(:username, :password)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  def self.from_token_request(request)
+    User.find_by(name: request.params[:auth][:username])
+  end
+
+  def authenticate(password)
+    # Do your custom authentication here.
+    # Return `true` if the auth should succeed, or `false` if it should fail.
+    User.find_by(password: password)
+  end
+end
