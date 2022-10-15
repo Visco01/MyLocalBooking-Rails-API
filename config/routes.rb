13c89faf9f925e412d3cfc4935a48b44fae0737e
Pietro Visconti <visconti373@gmail.com>
2022-10-15 12:18:21 +0200

    Added api_version V1

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
       get 'test_jwt', to: 'test_jwt#index'
       post "/auth", to: "user_token#create"
+
+      api_version(:module => "V1", :path => {:value => "api/v1"}, :default => true) do
+        #stuff
+      end
+
   end
 
 end
