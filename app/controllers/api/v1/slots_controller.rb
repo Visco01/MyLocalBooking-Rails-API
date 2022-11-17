5ffce34376aa4e8d791ce699b8090147c2653ef7
Pietro Visconti <visconti373@gmail.com>
2022-11-17 11:22:40 +0100

    Fixed again json response

--- app/controllers/api/v1/slots_controller.rb
+++ app/controllers/api/v1/slots_controller.rb
@@ -59,9 +59,7 @@ class Api::V1::SlotsController < Api::V1::BaseController
     @slot = Slot.find(params[:id])
     @slot.password_digest = request.params[:new_password]
     if @slot.save
-      render json: {
-               status: 200
-             }
+      render {status: "200"}, status: :created
     else
       render json: @slot.errors, status: :unprocessable_entity
     end
