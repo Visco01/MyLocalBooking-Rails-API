fc3a3b5944c530b5bb06f91b3b21d1d62319e49d
Pietro Visconti <visconti373@gmail.com>
2022-12-01 15:09:42 +0100

    Fixed previous commit

--- app/controllers/api/v1/providers_controller.rb
+++ app/controllers/api/v1/providers_controller.rb
@@ -47,7 +47,6 @@ class Api::V1::ProvidersController < Api::V1::BaseController
     end
   end
 
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_provider
