3533264a41bf7f35c992a3c69673c64182272c5f
Pietro Visconti <visconti373@gmail.com>
2022-12-20 11:44:49 +0100

    Changed relation between blueprints from 1-1 to 1-many

--- app/models/slot_blueprint.rb
+++ app/models/slot_blueprint.rb
@@ -1,5 +1,5 @@
 class SlotBlueprint < ApplicationRecord
   belongs_to :establishment
-  has_one :manual_slot_blueprint
-  has_one :periodic_slot_blueprint
+  has_many :manual_slot_blueprint
+  has_many :periodic_slot_blueprint
 end
--- app/serializers/slot_blueprint_serializer.rb
+++ app/serializers/slot_blueprint_serializer.rb
@@ -1,13 +1,13 @@
 class SlotBlueprintSerializer < ActiveModel::Serializer
-  attributes :id, :weekdays, :reservationlimit, :fromdate, :todate, :periodic_slot_blueprint, :manual_slot_blueprint
+  attributes :id, :weekdays, :reservationlimit, :fromdate, :todate, :periodic_slot_blueprints, :manual_slot_blueprints
   # has_one :establishment
 
-  def periodic_slot_blueprint
-    object.periodic_slot_blueprint
+  def periodic_slot_blueprints
+    PeriodicSlotBlueprint.where(slot_blueprint_id: object.id)
   end
 
-  def manual_slot_blueprint
-    object.manual_slot_blueprint
+  def manual_slot_blueprints
+    ManualSlotBlueprint.where(slot_blueprint_id: object.id)
   end
 
 end
