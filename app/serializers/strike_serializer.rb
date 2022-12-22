6e50e2b88cb1a08d8659a081906b14683d770bbd
Pietro Visconti <visconti373@gmail.com>
2022-12-22 17:35:33 +0100

    Fixed strikes serializer

--- app/serializers/strike_serializer.rb
+++ app/serializers/strike_serializer.rb
@@ -1,9 +1,9 @@
 class StrikeSerializer < ActiveModel::Serializer
-  attributes :id, :usercellphone, :count, :app_user
+  attributes :id, :usercellphone, :count
   has_one :provider
 
-  def app_user
-    app_user = AppUser.find_by(cellphone: object.usercellphone)
-    AppUserSerializer.new(app_user, without_serializer: true)
-  end
+  # def app_user
+  #   app_user = AppUser.find_by(cellphone: object.usercellphone)
+  #   AppUserSerializer.new(app_user, without_serializer: true)
+  # end
 end
