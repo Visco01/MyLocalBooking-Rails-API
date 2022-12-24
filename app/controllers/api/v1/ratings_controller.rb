52fc3076d41e842d1c0a87003493b72c48a0ed8a
Pietro Visconti <visconti373@gmail.com>
2022-12-24 18:04:04 +0100

    Changed ratings#create behaviour

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -15,12 +15,25 @@ class Api::V1::RatingsController < Api::V1::BaseController
 
   # POST /ratings
   def create
-    @rating = Rating.new(rating_params)
+    client_id = request.params[:client_id]
+    establishment_id = request.params[:establishment_id]
+    rating = request.params[:rating]
+    comment = request.params[:comment]
 
-    if @rating.save
-      render json: @rating, status: :created, location: @rating
+    existing_rating = Rating.find_by(client_id: client_id, establishment_id: establishment_id)
+
+    if existing_rating.nil?
+      create_new_rating(rating, client_id, establishment_id, comment, rating)
     else
-      render json: @rating.errors, status: :unprocessable_entity
+      existing_rating.comment = comment
+      existing_rating.rating = rating
+
+      if existing_rating.save
+        render json: { success: 'OK'}, status: :ok
+      else
+        render json: existing_rating.errors, status: :unprocessable_entity
+      end
+
     end
   end
 
@@ -47,5 +60,20 @@ class Api::V1::RatingsController < Api::V1::BaseController
     # Only allow a list of trusted parameters through.
     def rating_params
       params.require(:rating).permit(:rating, :comment, :establishment_id, :client_id)
+    end
+
+    def create_new_rating(client_id, establishment_id, comment, rating)
+      rating_obj = Rating.new
+      rating_obj.client_id = client_id
+      rating_obj.establishment_id = establishment_id
+      rating_obj.rating = rating
+      rating_obj.comment = comment
+
+      if rating_obj.save
+        render json: { success: 'OK'}, status: :ok
+      else
+        render json: rating_obj.errors, status: :unprocessable_entity
+      end
+
     end
 end
